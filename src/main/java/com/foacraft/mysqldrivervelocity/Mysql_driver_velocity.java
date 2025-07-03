a4b7c4f91494afbc7fa34bd5a37aa874445f3707
Score2 <score2.dev@gmail.com>
2025-07-03 22:32:05 +0800

    inject to the DriverManager

--- src/main/java/com/foacraft/mysqldrivervelocity/Mysql_driver_velocity.java
+++ src/main/java/com/foacraft/mysqldrivervelocity/Mysql_driver_velocity.java
@@ -6,12 +6,23 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import org.slf4j.Logger;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Plugin(
         id = "mysqldrivervelocity",
         name = "mysql-driver-velocity",
         version = BuildConstants.VERSION
 )
 public class Mysql_driver_velocity {
+
+    static {
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Inject
     private Logger logger;
